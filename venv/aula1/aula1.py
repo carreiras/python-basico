2803af8eaffb2a29b1be46d4cf452ed89d4d3b37
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-02 14:06:12 -0300

    Comentários de código em Python

---
+++ venv/aula1/aula1.py
@@ -0,0 +1,29 @@
+# Comentário
+print('Linha 1')
+print('Linha 2')
+# Falando do que se trata essa linha
+print('Linha 3')
+# Uma linha
+# Outra linha
+print('Linha 4')
+print('Linha 5')  # Isso é um comentário
+# print('Linha 6')
+# Final
+
+"""
+Comentário de múltiplas linhas
+
+print('Linha 1')
+print('Linha 2')
+print('Linha 3')
+
+"""
+
+'''
+Comentário de múltiplas linhas
+
+print('Linha 1')
+print('Linha 2')
+print('Linha 3')
+
+'''
