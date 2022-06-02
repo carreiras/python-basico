11bae87a2008a91d30ced428851bba13eaaa7ea3
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-02 19:48:32 -0300

    O comando print

---
+++ aula2/aula2.py
@@ -0,0 +1,14 @@
+print(123456)
+print('Ewerton', 'Carreira')
+print('Ewerton', 'Carreira', sep='-')
+
+print('Ewerton', 'Carreira', end='.')
+print('Joao', 'e', 'Maria', sep='-', end='')
+print('')
+
+'''
+755.260.900-19
+'''
+
+print('755', '260', '900', sep='.', end='-')
+print(19)
